7604f8bd43a1d5f4766f963be39cf9108218c70a
ndrew <awebrite@gmail.com>
2025-10-08 12:50:45 +0300

    basic unit tests for forms were written

--- django_app/matrix/models.py
+++ django_app/matrix/models.py
@@ -1,41 +1,324 @@
 from django.db import models
 
 
+
+class Table21(models.Model):
+    row1_column1 = models.IntegerField(default=0)
+    row2_column1 = models.IntegerField(default=0)
+
+
+class Table22(models.Model):
+    row1_column1 = models.IntegerField(default=0)
+    row1_column2 = models.IntegerField(default=0)
+    row2_column1 = models.IntegerField(default=0)
+    row2_column2 = models.IntegerField(default=0)
+
+
+class Table23(models.Model):
+    row1_column1 = models.IntegerField(default=0)
+    row1_column2 = models.IntegerField(default=0)
+    row1_column3 = models.IntegerField(default=0)
+    row2_column1 = models.IntegerField(default=0)
+    row2_column2 = models.IntegerField(default=0)
+    row2_column3 = models.IntegerField(default=0)
+
+
+class Table24(models.Model):
+    row1_column1 = models.IntegerField(default=0)
+    row1_column2 = models.IntegerField(default=0)
+    row1_column3 = models.IntegerField(default=0)
+    row1_column4 = models.IntegerField(default=0)
+
+    row2_column1 = models.IntegerField(default=0)
+    row2_column2 = models.IntegerField(default=0)
+    row2_column3 = models.IntegerField(default=0)
+    row2_column4 = models.IntegerField(default=0)
+
+
+class Table25(models.Model):
+    row1_column1 = models.IntegerField(default=0)
+    row1_column2 = models.IntegerField(default=0)
+    row1_column3 = models.IntegerField(default=0)
+    row1_column4 = models.IntegerField(default=0)
+    row1_column5 = models.IntegerField(default=0)
+
+
+    row2_column1 = models.IntegerField(default=0)
+    row2_column2 = models.IntegerField(default=0)
+    row2_column3 = models.IntegerField(default=0)
+    row2_column4 = models.IntegerField(default=0)
+    row1_column5 = models.IntegerField(default=0)
+
+
+class Table31(models.Model):
+    row1_column1 = models.IntegerField(default=0)
+    row2_column1 = models.IntegerField(default=0)
+    row3_column1 = models.IntegerField(default=0)
+
+
+class Table32(models.Model):
+    row1_column1 = models.IntegerField(default=0)
+    row1_column2 = models.IntegerField(default=0)
+    row2_column1 = models.IntegerField(default=0)
+    row2_column2 = models.IntegerField(default=0)
+    row3_column1 = models.IntegerField(default=0)
+    row3_column2 = models.IntegerField(default=0)
+
+
+class Table33(models.Model):
+    row1_column1 = models.IntegerField(default=0)
+    row1_column2 = models.IntegerField(default=0)
+    row1_column3 = models.IntegerField(default=0)
+    row2_column1 = models.IntegerField(default=0)
+    row2_column2 = models.IntegerField(default=0)
+    row2_column3 = models.IntegerField(default=0)
+    row3_column1 = models.IntegerField(default=0)
+    row3_column2 = models.IntegerField(default=0)
+    row3_column3 = models.IntegerField(default=0)
+
+
+class Table34(models.Model):
+    row1_column1 = models.IntegerField(default=0)
+    row1_column2 = models.IntegerField(default=0)
+    row1_column3 = models.IntegerField(default=0)
+    row1_column4 = models.IntegerField(default=0)
+
+    row2_column1 = models.IntegerField(default=0)
+    row2_column2 = models.IntegerField(default=0)
+    row2_column3 = models.IntegerField(default=0)
+    row2_column4 = models.IntegerField(default=0)
+
+    row3_column1 = models.IntegerField(default=0)
+    row3_column2 = models.IntegerField(default=0)
+    row3_column3 = models.IntegerField(default=0)
+    row3_column4 = models.IntegerField(default=0)
+
+
+class Table35(models.Model):
+    row1_column1 = models.IntegerField(default=0)
+    row1_column2 = models.IntegerField(default=0)
+    row1_column3 = models.IntegerField(default=0)
+    row1_column4 = models.IntegerField(default=0)
+    row1_column5 = models.IntegerField(default=0)
+
+    row2_column1 = models.IntegerField(default=0)
+    row2_column2 = models.IntegerField(default=0)
+    row2_column3 = models.IntegerField(default=0)
+    row2_column4 = models.IntegerField(default=0)
+    row2_column5 = models.IntegerField(default=0)
+
+    row3_column1 = models.IntegerField(default=0)
+    row3_column2 = models.IntegerField(default=0)
+    row3_column3 = models.IntegerField(default=0)
+    row3_column4 = models.IntegerField(default=0)
+    row3_column5 = models.IntegerField(default=0)
+
+
+class Table41(models.Model):
+    row1_column1 = models.IntegerField(default=0)
+    row2_column1 = models.IntegerField(default=0)
+    row3_column1 = models.IntegerField(default=0)
+    row4_column1 = models.IntegerField(default=0)
+
+
+class Table42(models.Model):
+    row1_column1 = models.IntegerField(default=0)
+    row1_column2 = models.IntegerField(default=0)
+    row2_column1 = models.IntegerField(default=0)
+    row2_column2 = models.IntegerField(default=0)
+    row3_column1 = models.IntegerField(default=0)
+    row3_column2 = models.IntegerField(default=0)
+    row4_column1 = models.IntegerField(default=0)
+    row4_column2 = models.IntegerField(default=0)
+
+
+class Table43(models.Model):
+    row1_column1 = models.IntegerField(default=0)
+    row1_column2 = models.IntegerField(default=0)
+    row1_column3 = models.IntegerField(default=0)
+    row2_column1 = models.IntegerField(default=0)
+    row2_column2 = models.IntegerField(default=0)
+    row2_column3 = models.IntegerField(default=0)
+    row3_column1 = models.IntegerField(default=0)
+    row3_column2 = models.IntegerField(default=0)
+    row3_column3 = models.IntegerField(default=0)
+    row4_column1 = models.IntegerField(default=0)
+    row4_column2 = models.IntegerField(default=0)
+    row4_column3 = models.IntegerField(default=0)
+
+
+class Table44(models.Model):
+    row1_column1 = models.IntegerField(default=0)
+    row1_column2 = models.IntegerField(default=0)
+    row1_column3 = models.IntegerField(default=0)
+    row1_column4 = models.IntegerField(default=0)
+
+    row2_column1 = models.IntegerField(default=0)
+    row2_column2 = models.IntegerField(default=0)
+    row2_column3 = models.IntegerField(default=0)
+    row2_column4 = models.IntegerField(default=0)
+
+    row3_column1 = models.IntegerField(default=0)
+    row3_column2 = models.IntegerField(default=0)
+    row3_column3 = models.IntegerField(default=0)
+    row3_column4 = models.IntegerField(default=0)
+
+    row4_column1 = models.IntegerField(default=0)
+    row4_column2 = models.IntegerField(default=0)
+    row4_column3 = models.IntegerField(default=0)
+    row4_column4 = models.IntegerField(default=0)
+
+
+class Table45(models.Model):
+    row1_column1 = models.IntegerField(default=0)
+    row1_column2 = models.IntegerField(default=0)
+    row1_column3 = models.IntegerField(default=0)
+    row1_column4 = models.IntegerField(default=0)
+    row1_column5 = models.IntegerField(default=0)
+
+    row2_column1 = models.IntegerField(default=0)
+    row2_column2 = models.IntegerField(default=0)
+    row2_column3 = models.IntegerField(default=0)
+    row2_column4 = models.IntegerField(default=0)
+    row2_column5 = models.IntegerField(default=0)
+
+    row3_column1 = models.IntegerField(default=0)
+    row3_column2 = models.IntegerField(default=0)
+    row3_column3 = models.IntegerField(default=0)
+    row3_column4 = models.IntegerField(default=0)
+    row3_column5 = models.IntegerField(default=0)
+    
+    row4_column1 = models.IntegerField(default=0)
+    row4_column2 = models.IntegerField(default=0)
+    row4_column3 = models.IntegerField(default=0)
+    row4_column4 = models.IntegerField(default=0)
+    row4_column5 = models.IntegerField(default=0)
+
+
+class Table51(models.Model):
+    row1_column1 = models.IntegerField(default=0)
+    row2_column1 = models.IntegerField(default=0)
+    row3_column1 = models.IntegerField(default=0)
+    row4_column1 = models.IntegerField(default=0)
+    row5_column1 = models.IntegerField(default=0)
+
+
+class Table52(models.Model):
+    row1_column1 = models.IntegerField(default=0)
+    row1_column2 = models.IntegerField(default=0)
+    row2_column1 = models.IntegerField(default=0)
+    row2_column2 = models.IntegerField(default=0)
+    row3_column1 = models.IntegerField(default=0)
+    row3_column2 = models.IntegerField(default=0)
+    row4_column1 = models.IntegerField(default=0)
+    row4_column2 = models.IntegerField(default=0)
+    row5_column1 = models.IntegerField(default=0)
+    row5_column2 = models.IntegerField(default=0)
+
+
+
+class Table53(models.Model):
+    row1_column1 = models.IntegerField(default=0)
+    row1_column2 = models.IntegerField(default=0)
+    row1_column3 = models.IntegerField(default=0)
+    row2_column1 = models.IntegerField(default=0)
+    row2_column2 = models.IntegerField(default=0)
+    row2_column3 = models.IntegerField(default=0)
+    row3_column1 = models.IntegerField(default=0)
+    row3_column2 = models.IntegerField(default=0)
+    row3_column3 = models.IntegerField(default=0)
+    row4_column1 = models.IntegerField(default=0)
+    row4_column2 = models.IntegerField(default=0)
+    row4_column3 = models.IntegerField(default=0)
+    row5_column1 = models.IntegerField(default=0)
+    row5_column2 = models.IntegerField(default=0)
+    row5_column3 = models.IntegerField(default=0)
+
+
+class Table54(models.Model):
+    row1_column1 = models.IntegerField(default=0)
+    row1_column2 = models.IntegerField(default=0)
+    row1_column3 = models.IntegerField(default=0)
+    row1_column4 = models.IntegerField(default=0)
+
+    row2_column1 = models.IntegerField(default=0)
+    row2_column2 = models.IntegerField(default=0)
+    row2_column3 = models.IntegerField(default=0)
+    row2_column4 = models.IntegerField(default=0)
+
+    row3_column1 = models.IntegerField(default=0)
+    row3_column2 = models.IntegerField(default=0)
+    row3_column3 = models.IntegerField(default=0)
+    row3_column4 = models.IntegerField(default=0)
+
+    row4_column1 = models.IntegerField(default=0)
+    row4_column2 = models.IntegerField(default=0)
+    row4_column3 = models.IntegerField(default=0)
+    row4_column4 = models.IntegerField(default=0)
+
+    row5_column1 = models.IntegerField(default=0)
+    row5_column2 = models.IntegerField(default=0)
+    row5_column3 = models.IntegerField(default=0)
+    row5_column4 = models.IntegerField(default=0)
+
+
+class Table55(models.Model):
+    row1_column1 = models.IntegerField(default=0)
+    row1_column2 = models.IntegerField(default=0)
+    row1_column3 = models.IntegerField(default=0)
+    row1_column4 = models.IntegerField(default=0)
+    row1_column5 = models.IntegerField(default=0)
+
+    row2_column1 = models.IntegerField(default=0)
+    row2_column2 = models.IntegerField(default=0)
+    row2_column3 = models.IntegerField(default=0)
+    row2_column4 = models.IntegerField(default=0)
+    row2_column5 = models.IntegerField(default=0)
+
+    row3_column1 = models.IntegerField(default=0)
+    row3_column2 = models.IntegerField(default=0)
+    row3_column3 = models.IntegerField(default=0)
+    row3_column4 = models.IntegerField(default=0)
+    row3_column5 = models.IntegerField(default=0)
+    
+    row4_column1 = models.IntegerField(default=0)
+    row4_column2 = models.IntegerField(default=0)
+    row4_column3 = models.IntegerField(default=0)
+    row4_column4 = models.IntegerField(default=0)
+    row4_column5 = models.IntegerField(default=0)
+
+    row5_column1 = models.IntegerField(default=0)
+    row5_column2 = models.IntegerField(default=0)
+    row5_column3 = models.IntegerField(default=0)
+    row5_column4 = models.IntegerField(default=0)
+    row5_column5 = models.IntegerField(default=0)
+
+
 class MatrixTable(models.Model):
-    table1_row_column_11 = models.IntegerField(default=0)
-    table1_row_column_12 = models.IntegerField(default=0)
-    table1_row_column_13 = models.IntegerField(default=0)
-    table1_row_column_14 = models.IntegerField(default=0)
-    table1_row_column_15 = models.IntegerField(default=0)
+    row_column_21_table = models.ManyToManyField(Table21)
+    row_column_22_table = models.ManyToManyField(Table22)
+    row_column_23_table = models.ManyToManyField(Table23)
+    row_column_24_table = models.ManyToManyField(Table24)
+    row_column_25_table = models.ManyToManyField(Table25)
 
-    table1_row_column_21 = models.IntegerField(default=0)
-    table1_row_column_22 = models.IntegerField(default=0)
-    table1_row_column_23 = models.IntegerField(default=0)
-    table1_row_column_24 = models.IntegerField(default=0)
-    table1_row_column_25 = models.IntegerField(default=0)
+    row_column_31_table = models.ManyToManyField(Table31)
+    row_column_32_table = models.ManyToManyField(Table32)
+    row_column_33_table = models.ManyToManyField(Table33)
+    row_column_34_table = models.ManyToManyField(Table34)
+    row_column_35_table = models.ManyToManyField(Table35)
 
-    table1_row_column_31 = models.IntegerField(default=0)
-    table1_row_column_32 = models.IntegerField(default=0)
-    table1_row_column_33 = models.IntegerField(default=0)
-    table1_row_column_34 = models.IntegerField(default=0)
-    table1_row_column_35 = models.IntegerField(default=0)
+    row_column_41_table = models.ManyToManyField(Table41)
+    row_column_42_table = models.ManyToManyField(Table42)
+    row_column_43_table = models.ManyToManyField(Table43)
+    row_column_44_table = models.ManyToManyField(Table44)
+    row_column_45_table = models.ManyToManyField(Table45)
 
-    table1_row_column_41 = models.IntegerField(default=0)
-    table1_row_column_42 = models.IntegerField(default=0)
-    table1_row_column_43 = models.IntegerField(default=0)
-    table1_row_column_44 = models.IntegerField(default=0)
-    table1_row_column_45 = models.IntegerField(default=0)
+    row_column_51_table = models.ManyToManyField(Table51)
+    row_column_52_table = models.ManyToManyField(Table52)
+    row_column_53_table = models.ManyToManyField(Table53)
+    row_column_54_table = models.ManyToManyField(Table54)
+    row_column_55_table = models.ManyToManyField(Table55)
 
-    table1_row_column_51 = models.IntegerField(default=0)
-    table1_row_column_52 = models.IntegerField(default=0)
-    table1_row_column_53 = models.IntegerField(default=0)
-    table1_row_column_54 = models.IntegerField(default=0)
-    table1_row_column_55 = models.IntegerField(default=0)
 
-    table2_row_column_11 = models.IntegerField(default=0)
-    table2_row_column_12 = models.IntegerField(default=0)
-    table2_row_column_13 = models.IntegerField(default=0)
-    table2_row_column_14 = models.IntegerField(default=0)
-    table2_row_column_15 = models.IntegerField(default=0)
 
 
